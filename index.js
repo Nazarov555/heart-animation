c9b1b6b2283abbc3dc9a92f8db9fa949884d9ec0
Кэскил Назаров <nzrvkeskil@mail.ru>
2022-08-08 15:25:46 +0300

    Second commit

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ bodyElement.addEventListener("mousemove", (event)=>{
         spanElement.remove();
     }, 800);
 });
+bodyElement.addEventListener("click", (event)=>{
+
+    const xPosition = event.offsetX;
+    const yPosition = event.offsetY;
+
+    const spanElement = document.createElement("span");
+
+    spanElement.style.left = xPosition + "px";
+    spanElement.style.top = yPosition + "px";
+
+    const size = Math.random()*400;
+
+    spanElement.style.width = size + "px";
+    spanElement.style.height = size + "px";
+
+    bodyElement.appendChild(spanElement);
+
+    setTimeout(()=>{
+        spanElement.remove();
+    }, 800);
+});
